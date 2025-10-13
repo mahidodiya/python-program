310838fd910c43ca2ddd22c4f88e3ec6f18db1cf
Mahi Dodiya <mahidodiya76@gmail.com>
2025-10-14 00:00:51 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,10 @@
+friends=["khushi","rajvi","dhruvi","palak","gaytri"]#creating list of friends
+print(friends)
+print(friends[0])
+print(friends[4])
+friends[0]="krisha"
+print(friends)
+del friends[4]
+print(friends)
+print(friends[0:3])
+print(friends[1:])
