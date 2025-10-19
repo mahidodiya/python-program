c35f85408ea3f6ca72bc725eba2ea6bf4d9aee15
Mahi Dodiya <mahidodiya76@gmail.com>
2025-10-19 21:45:30 +0530

    Add files via upload

---
+++ 2_tuple1.py
@@ -0,0 +1,13 @@
+family=('john','doe','emily','ani','lily','ani')
+f=family
+print(f)
+print(f[0])
+print(f[::])
+print(f[::2])
+#f.add(joe)
+#del f[0]
+#f[0]='max'
+#print(f)
+
+print(f.index('doe'))
+print(f.count('ani'))
